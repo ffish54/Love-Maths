204286b1f6c2bdb30c58f0889e1c29e57b756fd6
ffish <leo.bovey05@outlook.com>
2026-04-22 12:03:08 +0100

    added functions fo addition and calculate correct answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,26 +9,47 @@ document.addEventListener("DOMContentLoaded", function() {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
+                runGame(gameType);
             }
         });
 
     }
+
+    runGame("addition");
 });
 
 /**
  * The main game "loop", called when the script is first loaded and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     let  number1 = Math.floor(Math.random() * 25) + 1;
     let  number2 = Math.floor(Math.random() * 25) + 1;
+        if (gameType === "addition") {
+            displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
     
 }
 
 function checkAnswer() {
     
 }   
-
+/**
+ * Calculates the correct answer according to the displayed operands and operator.
+ */
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById("operand1").textContent);
+    let operand2 = parseInt(document.getElementById("operand2").textContent);
+    let operator = document.getElementById("operator").textContent;
+
+    if (operator === "+") {
+        return operand1 + operand2;
+    } else (
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    )
 
 }
 
@@ -41,6 +62,9 @@ function incrementWrongAnswer() {
 }
 
 function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "+";
     
 }
 
