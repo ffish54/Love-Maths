382b11fd7cde8f6b0eed79108a2992f3c280309c
ffish <leo.bovey05@outlook.com>
2026-04-27 12:03:36 +0100

    added multiplication questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,10 +24,15 @@ function runGame(gameType) {
     let  num2 = Math.floor(Math.random() * 25) + 1;
         if (gameType === "addition") {
             displayAdditionQuestion(num1, num2);
-    } else {
-        alert(`Unknown game type: ${gameType}`);
-        throw `Unknown game type: ${gameType}. Aborting!`;
-    }
+        }
+        else if (gameType === "multiply") {
+            displayMultiplyQuestion(num1, num2);
+        }
+        else {            
+            alert(`Unknown game type: ${gameType}`);
+            throw `Unknown game type: ${gameType}. Aborting!`;
+        }
+    
     
 }
 /**
@@ -61,6 +66,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"];
+
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -91,6 +99,10 @@ function displaySubtractQuestion(operand1, operand2) {
 }
 
 function displayMultiplyQuestion(operand1, operand2) {  
+
+    document.getElementById("operand1").innerText = operand1;
+    document.getElementById("operand2").innerText = operand2;
+    document.getElementById("operator").innerText = "x";
 
 }
 
